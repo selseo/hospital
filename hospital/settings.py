b5b4b87cbb1c924c5b18b62e294eeff172974691
Santacos <santacos@Santacoss-MacBook-Air.local>
2015-11-16 18:18:37 +0700

    add requirements

--- hospital/settings.py
+++ hospital/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     #third party library
     'djfrontend',
-    'djfrontend.skeleton',
+    #'djfrontend.skeleton',
 
     #for form + bootstrap form
     'crispy_forms',
