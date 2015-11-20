0285c0593e7880df6badabe1b3a0b182b1322b0f
Northnroro <northnroro@gmail.com>
2015-11-21 00:25:35 +0700

    can show table from foreign table (nurse view patient(visitInfo))

--- Visit/roles/nurse/views.py
+++ Visit/roles/nurse/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from ptregister.models import Patient
+from appointment.models import Appointment
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'nurse/index.html')
 def view(request):
-	return render(request, 'nurse/view.html',{'table':Patient.objects.all()})
+	return render(request, 'nurse/view.html',{'table':Appointment.objects.all()})
--- Visit/views.py
+++ Visit/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.hashers import make_password
 
 from ptregister.models import Patient
+from doctor_timetable.models import Doctor
 from appointment.models import Appointment
 
 import random
@@ -13,34 +14,34 @@ def index(request):
     return HttpResponse("All Indexes.")
 
 def seed(request):
-    d1=Doctor.objects.create(
-            drusername="test",
+    d1,xxx=Doctor.objects.get_or_create(
+            drusername="tes22t",
             drpassword=make_password(password="test",hasher='sha1'),
             drphone="021234567",
             drname="John",
             drsurname="Smith",
             drsex='m',
             drbirthdate="1990-12-12",
-            dridcard="1234567890123",
+            dridcard="12345617890123",
             draddress="aaa",
-            dremail="test@example.com"
-        )
-        d1.save()
-    d2=Doctor.objects.create(
-            drusername="t2222",
+            dremail="tes2tt@example.com"
+    )
+    d1.save()
+    d2,xxx=Doctor.objects.get_or_create(
+            drusername="t222212",
             drpassword=make_password(password="sdjshdfl",hasher='sha1'),
             drphone="99988877651",
             drname="Doctor",
             drsurname="Karn",
             drsex='f',
             drbirthdate="2004-3-8",
-            dridcard="9900990090909",
+            dridcard="99009902090909",
             draddress="xyz",
-            dremail="testxxxx@new.com"
-        )
-        d2.save()
+            dremail="testx2xxx@new.com"
+    )
+    d2.save()
     for i in range(0,10) :
-	    pt = Patient(ptusername=str(random.randrange(1,100)),
+        pt = Patient(ptusername=str(random.randrange(1,100)),
         ptpassword=make_password(password=str(random.randrange(0,100000)),hasher='sha1'),
         ptname='name'+str(random.randrange(1,100)),
         ptsurname='sur'+str(random.randrange(1,100)),
@@ -50,7 +51,9 @@ def seed(request):
         ptaddress=str(random.randrange(1,100)),
         ptemail=str(random.randrange(1,100)),
         ptnum=str(random.randrange(1,100)),
-        ptphone=str(random.randrange(1,100)),)
-	    pt.save()
+        ptphone=str(random.randrange(1,100)))
+        pt.save()
+        a = Appointment(doctor=random.choice([d1,d2]), patient=pt, symptom="DiE dIe :) JubJub", cause="StupiD")
+        a.save()
 
-	return render(request, 'seed.html')
+    return render(request, 'seed.html')
