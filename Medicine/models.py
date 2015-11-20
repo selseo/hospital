67797318c678ae2954cd83dd6bcee4937d3a3923
Bank <game255pang@gmail.com>
2015-11-20 18:28:32 +0700

    Add Medicine and Disease

---
+++ Medicine/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Medicine(models.Model)
+	name = models.CharField(max_length=300)
+	availability = models.BooleanField(default=True)
+
+	
