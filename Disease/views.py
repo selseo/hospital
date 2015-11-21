c15ac8e2dceb1e869abc1539ffa2628675781708
Bank <game255pang@gmail.com>
2015-11-21 18:11:14 +0700

    no message

--- Disease/views.py
+++ Disease/views.py
@@ -1,30 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse,HttpRequest
 from .models import Disease
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 # Create your views here.
+@csrf_exempt
 def index(request):
 	return render(request,'admin/disease.html',{'table':Disease.objects.all()})
-
+@csrf_exempt
 def addDisease(request):
-	return HttpResponse('')
-#     if request.method == 'POST':
-#     	name = request.POST["name"]
-#     	ICD10 = request.POST["ICD10"]
-#     	if name is not None and ICD10 is not None :
-#     		SNOMED = request.POST["SNOMED"]
-#     		DRG = request.POST["DRG"]
-#     		disease = Disease.objects.create(name=name,ICD10=ICD10,SNOMED=SNOMED,DRG=DRG)
-#     		disease.save()
-#     		return render(request,'admin/disease.html',{'table':Disease.objects.all(),'message':'Add Disease Success!'}})
-# 		else :
-# 			return render(request,'admin/disease.html',{'table':Disease.objects.all(),'message':'Add Disease Fail! Please recheck diseases name and ICD10'})
+    if request.method == 'POST':
+    	name = request.POST["name"]
+    	ICD10 = request.POST["ICD10"]
+    	if name and ICD10:
+    		SNOMED = request.POST["SNOMED"]
+    		DRG = request.POST["DRG"]
+    		disease = Disease.objects.create(name=name,ICD10=ICD10,SNOMED=SNOMED,DRG=DRG)
+    		disease.save()
+    		return render(request,'admin/disease.html',{'table':Disease.objects.all(),'message':'Add Disease Success!'})
+    	else :
+    		return render(request,'admin/disease.html',{'table':Disease.objects.all(),'message':'Add Disease Fail! Please recheck diseases name and ICD10'})
 
+@csrf_exempt
 def setAvailability(request):
-	return HttpResponse('')
-# 	if request.method == 'POST' :
-# 		ICD10 = request.POST["ICD10"]
-# 		availability = request.POST["availability"]
-# 		med = Medicine.sobjects.get(id=id)
-# 		med.availability = availability
-# 		med.save()
-# 		return HttpResponse('')		
+	# return HttpResponse('')
+	
+	ICD10 = request.POST["ICD10"]
+	availability = request.POST["availability"]
+	print (availability)
+	if availability == "true":
+		di = Disease.objects.get(ICD10=ICD10)
+		di.availability = True
+		di.save()
+	else : 
+		di = Disease.objects.get(ICD10=ICD10)
+		di.availability = False
+		di.save()
+
+	# .update(availability=availability)
+	# print (di.availability)
+	# di.availability = availability
+	# di.save()
+	# di = Disease.objects.filter(ICD10=ICD10)
+	# res = serializers.serialize('json',di)
+	return HttpResponse('')		
