3d6d47ee11c947be6089aab1bdfdc1bd6b000579
Bank <game255pang@gmail.com>
2015-11-21 14:37:37 +0700

    add url and template of disease

--- Medicine/views.py
+++ Medicine/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse,HttpRequest
-
+from .models import Medicine
 # Create your views here.
 def index(request):
-	return render()
+	return render(request,'index.html',{'table':Medicine.objects.all()})
 
 def addMedicine(request):
     if request.method == 'POST' :
@@ -12,9 +12,9 @@ def addMedicine(request):
 		if name is not None and table.count > 0 :
 			med = Medicine.objects.create(name=name)
 			med.save()
-			return render()
+			return render(request,'index.html',{'table':Medicine.objects.all(),'message':"Add Medicine Success!"})
 		else :
-			return render()
+			return render(request,'index.html',{'table':Medicine.objects.all(),'message':"Add Medicine Fail! Please recheck your medicine's name."})
 def setAvailability(request):
 	if request.method == 'POST' :
 		name = request.POST["name"]
