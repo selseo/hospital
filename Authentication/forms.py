93fb81e46ce29b4f6fbda7c3f6dd8717d9158737
Santacos <santacos@Santacoss-MacBook-Air.local>
2015-11-21 21:35:46 +0700

    register page :

--- Authentication/forms.py
+++ Authentication/forms.py
@@ -4,13 +4,26 @@ from Authentication.models import UserProfile,Patient
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control input-lg','placeholder':'Password'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Username'}),
+            'email': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Email'}),
+        }
+
+        
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('firstname','lastname','sex','idcard','phone')
+        widgets = {
+            'firstname': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Firstname'}),
+            'lastname': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Lastname'}),
+            'sex': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Sex'}),
+            'idcard': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'ID card'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control input-lg','placeholder':'Phone'}),
+        }
