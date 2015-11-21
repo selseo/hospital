a6760bc5c59c5d8bc10964a75232edd10ed46aec
peerapat40595 <peerapat40595@gmail.com>
2015-11-21 18:14:23 +0700

    Create Nurse form

--- Visit/roles/nurse/views.py
+++ Visit/roles/nurse/views.py
@@ -5,6 +5,9 @@ from datetime import datetime,timedelta
 from ptregister.models import Patient
 from appointment.models import Appointment
 from Visit.models import PatientVisitInfo
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import PatientVisitForms
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -17,5 +20,12 @@ def view(request):
 		 lastUpdate__year=datetime.now().year,
 		 status=0
 	)})
+
+@csrf_exempt
 def edit(request,num):
+	instance = get_object_or_404(PatientVisitInfo, appointment_id=num)
+	form = PatientVisitForms(request.POST or None, instance=instance)
+	if form.is_valid:
+		form.save()
+		return render(request, 'nurse/edit.html', {'num':num})
 	return render(request, 'nurse/edit.html', {'num':num})
