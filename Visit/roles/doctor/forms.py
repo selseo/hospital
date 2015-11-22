30a0a9497754c3d6ca618707db952058b7edb25a
peerapat40595 <peerapat40595@gmail.com>
2015-11-22 15:41:16 +0700

    doctor form1

--- Visit/roles/doctor/forms.py
+++ Visit/roles/doctor/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from Visit.models import PatientVisitInfo
+from Disease.models import Disease
+from django.db import models
+from django.db.models.query import QuerySet
 
 class PatientVisitDoctorForms(forms.ModelForm):
+    # diseases = forms.ModelMultipleChoiceField(queryset=Disease.objects.all(),label='Select disease')
     class Meta:
         model = PatientVisitInfo
-        fields = ('status','weight','height','pulse','systolic','diastolic')
+        fields = ['diseases']
+    def __init__(self, *args, **kwargs):
+         super(PatientVisitDoctorForms, self).__init__(*args, **kwargs)
+         self.fields['diseases'].widget = forms.widgets.CheckboxSelectMultiple()
+         self.fields['diseases'].queryset = Disease.objects.all()
+
