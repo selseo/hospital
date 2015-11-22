54f8d56c14d48f32da504c20fd882418e61d8ab2
pperfectionist <imania555@gmail.com>
2015-11-22 11:45:45 +0700

    Update appointment to use UserProfile instead of Patient

--- appointment/models.py
+++ appointment/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from Authentication.models  import Patient
+from Authentication.models  import UserProfile
 
 # Create your models here.
 class Department(models.Model):
@@ -77,7 +77,7 @@ class timeTable(models.Model):
 
 
 class Appointment(models.Model):
-    patient_id=models.ForeignKey(Patient)
+    patient_id=models.ForeignKey(UserProfile)
     timetable_id=models.ForeignKey(timeTable)
     symptom=models.CharField(max_length=100)
     cause=models.CharField(max_length=100)
