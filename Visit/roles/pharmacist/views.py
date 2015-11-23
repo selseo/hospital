37cf70d4e037a03a3b7f82bfe01f681707d4a245
Bank <game255pang@gmail.com>
2015-11-23 10:20:29 +0700

    change pharmacist redirect on visit

--- Visit/roles/pharmacist/views.py
+++ Visit/roles/pharmacist/views.py
@@ -18,7 +18,7 @@ from Authentication.models  import Patient
 def index(request):
 	#if request.user.is_authenticated():
 		#if getUserProfile(request.user).role==4://Pharmacist
-			return render(request, 'pharmacist/view.html',{'table':PatientVisitInfo.objects.filter(
+			return render(request, 'pharmacist/index.html',{'table':PatientVisitInfo.objects.filter(
 		 		lastUpdate__day=datetime.now().day,
 		 		lastUpdate__month=datetime.now().month,
 		 		lastUpdate__year=datetime.now().year,
