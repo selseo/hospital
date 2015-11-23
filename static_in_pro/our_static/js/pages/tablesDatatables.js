95c008843b8ef14f59b2806a246ac5e81713eed7
Santacos <santacos@Santacoss-MacBook-Air.local>
2015-11-23 11:56:24 +0700

    view user list page

--- static_in_pro/our_static/js/pages/tablesDatatables.js
+++ static_in_pro/our_static/js/pages/tablesDatatables.js
@@ -12,11 +12,12 @@ var TablesDatatables = function() {
             App.datatables();
 
             /* Initialize Datatables */
-            $('#example-datatable').dataTable({
-                "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 1, 5 ] } ],
-                "iDisplayLength": 10,
-                "aLengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]]
-            });
+            // $('#example-datatable').dataTable({
+            //     "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 1, 4 ] } ],
+            //     "iDisplayLength": 10,
+            //     "aLengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]]
+            // });
+            $('#example-datatable').dataTable();
 
             /* Add placeholder attribute to the search input */
             $('.dataTables_filter input').attr('placeholder', 'Search');
