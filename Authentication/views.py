8876039625770c51fb2b69f278b1e1d44905e1d6
selseo <silversoul.sevendeadlysins@gmail.com>
2015-11-25 16:02:27 +0700

    can edit role

--- Authentication/views.py
+++ Authentication/views.py
@@ -541,6 +541,19 @@ def viewuser(request, userl_slug):
             department=Doctor.objects.get(userprofile=userl).department
         else :
             department=None
+        if role==0:
+            role_is="Patient"
+        if role==1:
+            role_is="Doctor"
+        if role==2:
+            role_is="Nurse"
+        if role==3:
+            role_is="Officer"
+        if role==4:
+            role_is="Pharmacist"
+        if role==5:
+            role_is="Admin"
+
 
     except UserProfile.DoesNotExist :
         return HttpResponseRedirect('/default/viewuserlist/')
@@ -549,7 +562,7 @@ def viewuser(request, userl_slug):
             'admin/viewuser.html',
             {'firstname':firstname,
             'lastname':lastname,
-            'role':role,
+            'role':role_is,
             'sex':sex,
             'idcard':idcard,
             'birthdate':birthdate,
@@ -592,7 +605,7 @@ def edituser(request, userl_slug):
         admin_user_form = AdminCreateUser(data=request.POST)
         admin_doctor_form= AdminCreateDoctor(data=request.POST)
         # If the two forms are valid...
-        if admin_user_form.is_valid() and admin_doctor_form.is_valid():
+        if admin_user_form.is_valid() :
             # Save the user's form data to the database.
             #user = user_form.save()
             userprofile = userl
@@ -605,10 +618,12 @@ def edituser(request, userl_slug):
             # Once hashed, we can update the user object.
             if(request.POST['password']):
                 userAccount.set_password(request.POST['password'])
-            userDoc.department=request.POST['department']
+            if userprofile.role==1:
+                userDoc.department=request.POST['department']
+                userDoc.save()
             userAccount.save()
             userprofile.save()
-            userDoc.save()
+            
             # Now sort out the UserProfile instance.
             # Since we need to set the user attribute ourselves, we set commit=False.
             # This delays saving the model until we're ready to avoid integrity problems.
@@ -647,6 +662,7 @@ def edituser(request, userl_slug):
         if user_info['role']==1:
             admin_doctor_form= AdminCreateDoctor(initial={
                 'department': user_info['department']})
+
 
 
     if user_info['role']==1:        
