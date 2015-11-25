d59d6b3537744ff51b8ffa9796db15fe892f9d22
Santacos <santacos@Santacoss-MacBook-Air.local>
2015-11-25 21:44:25 +0700

    ..

--- Authentication/views.py
+++ Authentication/views.py
@@ -132,9 +132,11 @@ def register(request):
     # A boolean value for telling the template whether the registration was successful.
     # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
+    
 
     # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
+        
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
@@ -183,6 +185,7 @@ def register(request):
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.
     else:
+    
         user_form = UserForm()
         profile_form = UserProfileForm()
         patient_form = PatientProfile()
@@ -191,7 +194,7 @@ def register(request):
     return render(request,
             'theme/register.html',
             {'user_form': user_form, 'profile_form': profile_form, 'patient_form':patient_form,'registered': registered} )
-
+    
 def some_view(request):
     if not request.user.is_authenticated():
         return HttpResponse("You are logged in.")
@@ -538,6 +541,19 @@ def viewuser(request, userl_slug):
             department=Doctor.objects.get(userprofile=userl).department
         else :
             department=None
+        if role==0:
+            role_is="Patient"
+        if role==1:
+            role_is="Doctor"
+        if role==2:
+            role_is="Nurse"
+        if role==3:
+            role_is="Officer"
+        if role==4:
+            role_is="Pharmacist"
+        if role==5:
+            role_is="Admin"
+
 
     except UserProfile.DoesNotExist :
         return HttpResponseRedirect('/default/viewuserlist/')
@@ -546,7 +562,7 @@ def viewuser(request, userl_slug):
             'admin/viewuser.html',
             {'firstname':firstname,
             'lastname':lastname,
-            'role':role,
+            'role':role_is,
             'sex':sex,
             'idcard':idcard,
             'birthdate':birthdate,
@@ -589,7 +605,7 @@ def edituser(request, userl_slug):
         admin_user_form = AdminCreateUser(data=request.POST)
         admin_doctor_form= AdminCreateDoctor(data=request.POST)
         # If the two forms are valid...
-        if admin_user_form.is_valid() and admin_doctor_form.is_valid():
+        if admin_user_form.is_valid() :
             # Save the user's form data to the database.
             #user = user_form.save()
             userprofile = userl
@@ -602,10 +618,12 @@ def edituser(request, userl_slug):
             # Once hashed, we can update the user object.
             if(request.POST['password']):
                 userAccount.set_password(request.POST['password'])
-            userDoc.department=request.POST['department']
+            if userprofile.role==1:
+                userDoc.department=request.POST['department']
+                userDoc.save()
             userAccount.save()
             userprofile.save()
-            userDoc.save()
+            
             # Now sort out the UserProfile instance.
             # Since we need to set the user attribute ourselves, we set commit=False.
             # This delays saving the model until we're ready to avoid integrity problems.
@@ -644,6 +662,7 @@ def edituser(request, userl_slug):
         if user_info['role']==1:
             admin_doctor_form= AdminCreateDoctor(initial={
                 'department': user_info['department']})
+
 
 
     if user_info['role']==1:        
